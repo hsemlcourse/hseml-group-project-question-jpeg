c15a3aa81e5935b0366e3d7907f158bbce74d321
igormikhailov <igormikhailov@yandex-team.ru>
2026-04-25 16:46:55 +0300

    linter

--- src/train_baseline.py
+++ src/train_baseline.py
@@ -1,7 +1,8 @@
+import joblib
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import pandas as pd
 
 X_train = np.load("data/processed/X_train_processed.npy")
 X_val = np.load("data/processed/X_val_processed.npy")
@@ -17,7 +18,7 @@ mae = mean_absolute_error(y_val, y_pred)
 rmse = np.sqrt(mean_squared_error(y_val, y_pred))
 r2 = r2_score(y_val, y_pred)
 
-print(f"Baseline Linear Regression:")
+print("Baseline Linear Regression:")
 print(f"MAE: {mae:.2f}")
 print(f"RMSE: {rmse:.2f}")
 print(f"R2: {r2:.4f}")
@@ -25,5 +26,4 @@ print(f"R2: {r2:.4f}")
 results = pd.DataFrame([{'model': 'LinearRegression', 'MAE': mae, 'RMSE': rmse, 'R2': r2}])
 results.to_csv("report/baseline_results.csv", index=False)
 
-import joblib
 joblib.dump(model, "models/baseline_model.pkl")
